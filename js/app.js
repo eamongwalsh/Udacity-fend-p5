739e8610650ad6599e5b5dbc1f9c864dfb8b5028
Eamon Walsh <eamongwalsh@gmail.com>
2016-03-23 12:29:28 +0000

    Refactored code a lot to get Knockout filter working with map markers

--- js/app.js
+++ js/app.js
@@ -1,91 +1,126 @@
 /* Knockoutjs - Udacity project no. 6 - Neighbourhood map */
 $(document).ready(function() {
-/* This bit works - basic.... verified API key  */
+     
+var mapOptions = {
+    center: new google.maps.LatLng(53.273949, -9.050516),
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
 
-    var mapOptions = {
-        center: new google.maps.LatLng(53.27, -9.053),
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
+var map = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
   
-    //Create Markers
-
-    var markers = [];
-
-    markers[0] = new google.maps.Marker({
-        position: new google.maps.LatLng(53.272401, -9.053328),
-        map: map
-    });
-
-    markers[1] = new google.maps.Marker({
-        position: new google.maps.LatLng(53.271354, -9.053933),
-        map: map,
-    });
-
-    //Create Infowindows
-    
-    var infoWindows = [];
-    var contentStrings = [];
-    
-    contentStrings[0] = "<div style='height: 150px; width: 150px;'><img src='http://www.menupages.ie/images/550x344/6053_the%20kings%20head.JPG' height='100' width='100'><p>The Kings Head!</p></div>";
-    
-    contentStrings[1] = "<div style='height: 150px; width: 150px;'><img src='http://pubsdirect.ie/images/pub_images/360x220/289.jpg' height='100' width='100'><p>The Quays medival pub!</p></div>";
-
-    for (var i = 0; i < contentStrings.length; i++) {
-        infoWindows[i] = new google.maps.InfoWindow({
-            content: contentStrings[i]
-        });
+//Determines if markers should be visible
+//This function is passed in the knockout viewModel function
+function setAllMap() {
+  for (var i = 0; i < markers.length; i++) {
+    if(markers[i].showMe === true) {
+    markers[i].holdMarker.setMap(map);
+    } else {
+    markers[i].holdMarker.setMap(null);
     }
+  }
+}
     
-    google.maps.event.addListener(markers[0], 'click', function() {
-        infoWindows[0].open(map, markers[0]);
-    });
- 
-    google.maps.event.addListener(markers[1], 'click', function() {
-        infoWindows[1].open(map, markers[1]);
-    });
-
-    function createMarker(latitude, longitude, name) {
-        var marker = new google.maps.Marker({
-          map: map,
-          position: new google.maps.LatLng(latitude, longitude)
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(name);
-          infowindow.open(map, this);
-        });
-    }
-
-
-    var locations = [{
-        name: "Kings Head",
-        facts: "17th century pub with nightly live music!",
-        marker: markers[0],
-        lat: 53.272401,
-        lon: -9.053328
-    }, {
-        name: "The Quays",
-        facts: "Unique pub with medieval church fittings.",
-        marker: markers[1],
-        lat: 53.271354,
-        lon: -9.053933
+    //Provides information for the markers
+var markers = [
+    {   
+    title: "The Kings Head",
+    lat: 53.272401, 
+    lng: -9.053328,
+    description: "17th century pub with nightly live music!",
+    visible: ko.observable(true),
+    showMe: true
+    },
+    {   
+    title: "The Quays",
+    lat: 53.271354, 
+    lng: -9.053933,
+    description: "Unique pub with medieval church fittings.",
+    visible: ko.observable(true),
+    showMe: true
+    },
+    {   
+    title: "O'Connells",
+    lat: 53.274809, 
+    lng: -9.048338,
+    description: "Right there on Eyre Square.",
+    visible: ko.observable(true),
+    showMe: true
+    },
+    {   
+    title: "The Skeffington Arms",
+    lat: 53.274256, 
+    lng: -9.049885,
+    description: "The student pub of Galway City!",
+    visible: ko.observable(true),
+    showMe: true
     }];
 
-    //Knockout ViewModel
+setMarkers(markers);
 
-    var viewModel = {
-        query: ko.observable('')
-    };
+setAllMap();
 
-    viewModel.locations = ko.dependentObservable(function() {
-        var search = this.query().toLowerCase();
-        return ko.utils.arrayFilter(locations, function(location) {
-            return location.name.toLowerCase().indexOf(search) >= 0;
+//Sets the markers on the map within the initialize function
+//Sets the infoWindows to each individual marker
+//The markers are inidividually set using a for loop
+function setMarkers(location) {
+    
+    for(i=0; i<location.length; i++) {
+        location[i].holdMarker = new google.maps.Marker({
+          position: new google.maps.LatLng(location[i].lat, location[i].lng),
+          map: map,
+          title: location[i].title,
+          icon: {
+            url: 'img/marker.png',
+            size: new google.maps.Size(25, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(12.5, 40)
+            },
+          shape: {
+            coords: [1,25,-40,-25,1],
+            type: 'poly'
+          }  
         });
-    }, viewModel);
+
+        //Binds infoWindow content to each marker
+        location[i].contentString = location[i].title + '</strong><br><p>' + location[i].description + '<br>' + '</p>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: markers[i].contentString
+        });
+
+        //Click marker to view infoWindow
+            //zoom in and center location on click
+        new google.maps.event.addListener(location[i].holdMarker, 'click', (function(marker, i) {
+          return function() {
+            infowindow.setContent(location[i].contentString);
+            infowindow.open(map,this);
+          }; 
+        })(location[i].holdMarker, i));
+    }
+}
+    
+    
+//Knockout ViewModel
+
+var viewModel = {
+    query: ko.observable('')
+};
+
+viewModel.markers = ko.dependentObservable(function() {
+    var self = this;
+    var search = self.query().toLowerCase();
+    return ko.utils.arrayFilter(markers, function(marker) {
+    if (marker.title.toLowerCase().indexOf(search) >= 0) {
+            marker.showMe = true;
+            return marker.visible(true);
+        } else {
+            marker.showMe = false;
+            setAllMap();
+            return marker.visible(false);
+        }
+    });       
+}, viewModel);
 
 
 ko.applyBindings(viewModel);
