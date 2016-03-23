fbbfe44a84bd1738ae14df6cd4836aedf1cddd3a
Eamon Walsh <eamongwalsh@gmail.com>
2016-03-23 10:21:06 +0000

    Refactored code to include locations and Knockout filter

--- js/app.js
+++ js/app.js
@@ -1,69 +1,93 @@
 /* Knockoutjs - Udacity project no. 6 - Neighbourhood map */
+$(document).ready(function() {
+/* This bit works - basic.... verified API key  */
 
-/* This bit works - basic.... verified API key  
-     var myMap;
-      function initMap() {
-        myMap = new google.maps.Map(document.getElementById('mapDiv'), {
-          center: {lat: 53.27, lng: -9.053},
-          zoom: 12
-        });
-      }*/
+    var mapOptions = {
+        center: new google.maps.LatLng(53.27, -9.053),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-    // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-    var viewModel = {
-        myValue: ko.observable("What are you looking for?"),
-        mySubmit: function() {
-
-            //alert("submitting: " + this.myValue());
-            initMap(this.myValue());
-        } //end mySubmit function
-
-    }; //end viewModel
-        
-      var map;
-      var infowindow;
-      //var placeType = 'bar';
-
-      function initMap(placeType) {
-        var galway = {lat: 53.27, lng: -9.053};
-
-        map = new google.maps.Map(document.getElementById('mapDiv'), {
-          center: galway,
-          zoom: 15
-        });
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: galway,
-          radius: 500,
-          type: [placeType]
-        }, callback);
-      }
-
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
+    var map = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
   
-        
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
+    //Create Markers
+
+    var markers = [];
+
+    markers[0] = new google.maps.Marker({
+        position: new google.maps.LatLng(53.272401, -9.053328),
+        map: map
+    });
+
+    markers[1] = new google.maps.Marker({
+        position: new google.maps.LatLng(53.271354, -9.053933),
+        map: map,
+    });
+
+    //Create Infowindows
+    
+    var infoWindows = [];
+    var contentStrings = [];
+    
+    contentStrings[0] = "<div style='height: 150px; width: 150px;'><img src='http://www.menupages.ie/images/550x344/6053_the%20kings%20head.JPG' height='100' width='100'><p>The Kings Head!</p></div>";
+    
+    contentStrings[1] = "<div style='height: 150px; width: 150px;'><img src='http://pubsdirect.ie/images/pub_images/360x220/289.jpg' height='100' width='100'><p>The Quays medival pub!</p></div>";
+
+    for (var i = 0; i < contentStrings.length; i++) {
+        infoWindows[i] = new google.maps.InfoWindow({
+            content: contentStrings[i]
+        });
+    }
+    
+    google.maps.event.addListener(markers[0], 'click', function() {
+        infoWindows[0].open(map, markers[0]);
+    });
+ 
+    google.maps.event.addListener(markers[1], 'click', function() {
+        infoWindows[1].open(map, markers[1]);
+    });
+
+    function createMarker(latitude, longitude, name) {
         var marker = new google.maps.Marker({
           map: map,
-          position: place.geometry.location
+          position: new google.maps.LatLng(latitude, longitude)
         });
 
         google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
+          infowindow.setContent(name);
           infowindow.open(map, this);
         });
-      }
+    }
+
+
+    var locations = [{
+        name: "Kings Head",
+        facts: "17th century pub with nightly live music!",
+        marker: markers[0],
+        lat: 53.272401,
+        lon: -9.053328
+    }, {
+        name: "The Quays",
+        facts: "Unique pub with medieval church fittings.",
+        marker: markers[1],
+        lat: 53.271354,
+        lon: -9.053933
+    }];
+
+    //Knockout ViewModel
+
+    var viewModel = {
+        query: ko.observable('')
+    };
+
+    viewModel.locations = ko.dependentObservable(function() {
+        var search = this.query().toLowerCase();
+        return ko.utils.arrayFilter(locations, function(location) {
+            return location.name.toLowerCase().indexOf(search) >= 0;
+        });
+    }, viewModel);
+
 
 ko.applyBindings(viewModel);
+    
+});
